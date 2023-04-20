19e64f9e2a058dc36955af213e99a2de3bc1fd14
wangtianshu <wang@tianshu.me>
2023-04-20 16:13:47 +0800

    Add moment

---
+++ Casks/moment.rb
@@ -0,0 +1,11 @@
+cask "moment" do
+  version "147"
+  sha256 "a34a2007debae0afd79d2ee0ed4b4e27538bfdb47e512a467b952005f2302d8d"
+
+  url "https://fireball.studio/media/uploads/files/Moment-#{version}.dmg"
+  name "Moment"
+  desc "Menu Bar and Notification Center based countdown app"
+  homepage "https://fireball.studio/moment"
+
+  app "Moment.app"
+end
